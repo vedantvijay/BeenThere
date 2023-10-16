b8dcabe505eaecfd6be61a08599a284e7810bec8
Jared Jones <84288718+JaredDanielJones@users.noreply.github.com>
2023-10-16 09:42:14 -0500

    Initial Commit

---
+++ BeenThere/BeenThereApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeenThereApp.swift
+//  BeenThere
+//
+//  Created by Jared Jones on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct BeenThereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
