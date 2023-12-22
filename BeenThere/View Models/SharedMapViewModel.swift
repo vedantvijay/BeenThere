9d85c9588bb1e5f168f1e560d16dfbc58370cd11
Jared Jones <84288718+JaredDanielJones@users.noreply.github.com>
2023-12-21 19:14:43 -0600

    1.0.16

--- BeenThere/View Models/SharedMapViewModel.swift
+++ BeenThere/View Models/SharedMapViewModel.swift
@@ -24,7 +24,10 @@ class SharedMapViewModel: TemplateMapViewModel {
         mapView?.isOpaque = false
         mapView?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         mapView?.location.options.puckType = .puck2D(.makeDefault(showBearing: true))
+        mapView?.ornaments.scaleBarView.isHidden = true
         self.annotationManager = mapView?.annotations.makePointAnnotationManager()
+        mapView?.ornaments.logoView.alpha = 0.1
+        mapView?.ornaments.attributionButton.alpha = 0.1
         addGridlinesToMap()
         centerMapOnLocation(location: locationManager.location ?? CLLocation(latitude: 50, longitude: 50))
     }
